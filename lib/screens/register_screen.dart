55147e67c0cdfff99fb4242e2e9ca140cf7546db
ericksebastian2003 <151951589+ericksebastian2003@users.noreply.github.com>
2024-12-19 22:46:08 -0500

    Actualización del Registro - Creación y Manejo de Formularios

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -10,7 +10,8 @@ class RegisterScreen extends StatefulWidget {
 }
 class _RegisterScreenState extends State<RegisterScreen>{
   //Para manejar los cammpos del formulario
-  
+  final _formKey = GlobalKey<FormState>();
+  String? _rutaSelect;
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _confirmPasswordController = TextEditingController();
@@ -21,14 +22,14 @@ class _RegisterScreenState extends State<RegisterScreen>{
   //Funcion para el regsitro
 
   Future<void> _registerWithEmailAndPassword() async{
-if(_emailController.text.isEmpty || _nameController.text.isEmpty || _passwordController.text.isEmpty || _confirmPasswordController.text.isEmpty){
-  _showErrorDialog("Por favor , todos los cmapos son obligatorios");
-  return;
-}
-if(_passwordController.text != _confirmPasswordController.text){
-  _showErrorDialog("Las contraseñas no coinciden");
-  return;
-}
+    if(!_formKey.currentState!.validate()){
+      return;
+    }
+
+    if(_passwordController.text != _confirmPasswordController.text){
+      _showErrorDialog("Las contraseñas no coinciden");
+      return;
+    }
 //Cambio de estado 
 setState(() {
   _isLoading = true;  
@@ -46,11 +47,13 @@ catch(e){
 
 }
 finally{
+  if(mounted){
   setState(() {
    // Cambiamos el estado para quitar el indicador de carga
     _isLoading = false;
 
   });
+}
 }
 }
 void _showErrorDialog(String message){
@@ -72,10 +75,19 @@ void _showErrorDialog(String message){
     ),
   );
 }
-  
 
   //Interfaz de usuario
   @override
+  //Limpiar los cmapos
+
+  void dispose(){
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    _nameController.dispose();
+
+    super.dispose();
+  }
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -84,59 +96,116 @@ void _showErrorDialog(String message){
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _nameController,
-              decoration: const InputDecoration(
-                labelText: "Nombres completos",
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.person)
-              ),
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: "Correo electrónico",
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.mail)
-              ),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              obscureText: true,
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: "Contraseña",
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.lock)
-              ),
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              obscureText: true,
-              controller: _confirmPasswordController,
-              decoration: const InputDecoration(
-                labelText: "Confirmar Contraseña",
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.lock)
-              ),
-            ),
-            const SizedBox(height: 18),
-            ElevatedButton(
-              onPressed: _isLoading ? null : _registerWithEmailAndPassword, 
-              child: _isLoading  
-                ? const CircularProgressIndicator(color: Colors.deepPurple)
-                : const Text("Registrarse"),
+        child: Form(
+          key : _formKey,
+          child : Column(
+            children: [
+                TextFormField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(
+                    labelText: "Nombres completos",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person)
+                  ),
+                  //Validaciones
+                  validator: (value){
+                    if(value == null || value.isEmpty){
+                      return "Ingrese su nombre completo";
+                    }
+                    return null;
+                  },
                 ),
-          ],
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    labelText: "Correo electrónico",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.mail)
+                  ),
+                  validator: (value) {
+                    if(value == null || value.isEmpty){
+                        return "Ingrese su correo electrónico";
+                    }
+                    return null;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  obscureText: true,
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    labelText: "Contraseña",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.lock)
+                  ),
+                  validator: (value) {
+                    if(value == null || value.isEmpty){
+                        return "Ingrese su contraseña";
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  obscureText: true,
+                  controller: _confirmPasswordController,
+                  decoration: const InputDecoration(
+                    labelText: "Confirmar Contraseña",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.lock)
+                  ),
+                  validator: (value){
+                    if(value != _passwordController.text){
+                      return "Las contraseñas no coinciden";
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                DropdownButtonFormField(
+                  value: _rutaSelect,
+                  hint: const Text("Seleccione su ruta"),
+                  items: [
+                    "Ruta 1: Cárcelen",
+                    "Ruta 2: Pisuli",
+                    "Ruta 3: Ejido" ,
+                  ].map((option)=> DropdownMenuItem(
+                      value: option,
+                      child: Text(option),
+                    )).toList(),
+                  onChanged: (value){
+                    if(value != null){
+                    setState(() {
+                      _rutaSelect = value;
+                    });
+                  }
+                  },
+                  validator: (value) =>
+                    value == null ? "Seleccione una ruta" : null,
+                    
+                ),
+
+                
+                const SizedBox(height: 18),
+                ElevatedButton(
+                  onPressed: _isLoading 
+                      ? null 
+                      : () {
+                        if(_formKey.currentState!.validate()){
+                         _registerWithEmailAndPassword();
+                        }
+                      } ,
+                  child: _isLoading  
+                    ? const CircularProgressIndicator(color: Colors.deepPurple)
+                    : const Text("Registrarse"),
+                    ),
+              ],
         ),
         
         ),
-
+      ),
     );
   }
 
